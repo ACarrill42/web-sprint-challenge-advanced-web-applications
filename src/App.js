8b1cf04c5d62b882d6f0513d1c38cda83b6d4118
ACarrill42 <austincarrill@gmail.com>
2021-06-13 14:13:37 -0500

    finished axiosAuth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from "./components/BubblePage";
 import Login from "./components/Login";
 import "./styles.scss";
 
@@ -12,8 +13,11 @@ function App() {
           Color Picker Sprint Challenge
           <a data-testid="logoutButton" href="#">logout</a>
         </header> 
+        <Switch>
+          <PrivateRoute exact path = "/BubblePage" component = {BubblePage}/>
+          <Route exact path="/" component={Login} />          
+        </Switch>
 
-        <Route exact path="/" component={Login} />
       </div>
     </Router>
   );
